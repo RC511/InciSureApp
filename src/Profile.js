07ade1526cb2886e1a609784710bb177a7b4de07
RC511 <ryokusnadi2@gmail.com>
2023-09-27 22:29:40 +0800

    Profile page update

--- src/Profile.js
+++ src/Profile.js
@@ -1,25 +1,67 @@
+import './Profile.css';
 import logo from './logo.svg';
-import {Link} from 'react-router-dom';
+import profilePic from './assets/img_avatar.png'
+import rectangle from './assets/Rectangle.png'
 import NavBarItem from './NavBarItem';
+import './NavBar.css';
 
-function Profile() {
-    return (
-        <div className="App">
+
+export default function Profile() {
+    return(
+        <div class="backgr">
             <br></br>
             <div class="navBar">
                 {NavBarItem(4)}
             </div>
-            <header>
-                <Link to='/'>Back home</Link>
-            </header>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    About us.
-                </p>
-            </header>
-        </div>
-    );
-}
+            <div class="mainarea">
+                <div class="pfp">
+                    <img src={profilePic} className="App-profilePic" alt="profilePic"/>
+                    testin
+                </div>
+                <div class="info">
+                    <div>
+                        User Information
+                    </div>
+                    <div class="infbox">
+                        <div>
+                            <p className="inftitles">Name</p>
+                            <p className="inftitles">Birthday</p>
+                            <p className="inftitles">Phone</p>
+                            <p className="inftitles">Email</p>
+                            <p className="inftitles">Sex</p>
+                            <p className="inftitles">Age</p>
+                            <p className="inftitles">Emergency contact</p>
+                        </div>
+                    </div>
+                </div>
 
-export default Profile;
+            </div>
+
+
+            
+            {/* <p className="App-title">
+            Hello World
+            </p>
+            <img src={rectangle} className="App-rectangle" alt="rectangle"/> */}
+            
+
+            
+
+
+            
+                {/* <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Learn React
+                </a> */}
+
+         </div>
+    )
+}
