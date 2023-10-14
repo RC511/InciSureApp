912409fa681c212e41dbcfe0f4cbc5a2b15d075e
RC511 <ryokusnadi2@gmail.com>
2023-10-14 19:37:30 +0800

    Updates to Login page
    Added "Remember credentials" checkbox
    Changed username into email
    Added EmailJS package

--- src/ForgotPass.js
+++ src/ForgotPass.js
@@ -1,5 +1,7 @@
 import '@fontsource/lexend';
-import { useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import emailjs from '@emailjs/browser';
 
 async function findUser(name) {
     let found = false;
@@ -32,6 +34,31 @@ export default function ForgotPass() {
     const [username, setUserName] = useState();
     const [dispErr, setDispErr] = useState(false);
   
+    // const emailRef = useRef<HTMLInputElement>();
+    // const nameRef = useRef<HTMLInputElement>();
+    // const [loading, setLoading] = useState(false);
+
+    // useEffect(() => emailjs.init("YOUR-PUBLIC-KEY-HERE"), []);
+    // const handleSubmit = async (e) => {
+    //   e.preventDefault();
+    //   const serviceId = "YOUR-SERVICE-ID-HERE";
+    //   const templateId = "YOUR-TEMPLATE-ID-HERE"";
+    //   try {
+    //     setLoading(true);
+    //     await emailjs.send(serviceId, templateId, {
+    //     name: nameRef.current.value,
+    //       recipient: emailRef.current.value
+    //     });
+    //     alert("email successfully sent check inbox");
+    //   } catch (error) {
+    //     console.log(error);
+    //   } finally {
+    //     setLoading(false);
+    //   }
+    // };
+    // https://www.telerik.com/blogs/sending-emails-react-app-using-emailjs
+    // https://www.npmjs.com/package/@emailjs/browser
+
     const handleSubmit = async e => {
       e.preventDefault();
       const found = await findUser(username);
@@ -49,16 +76,19 @@ export default function ForgotPass() {
     return(
         <div class="logDiv">
             <br></br>
-            <h1 class="h1Login">Enter your Email</h1>
+            <h1 class="h1Login">Forgot your password</h1>
             <form onSubmit={handleSubmit}>
                 <label class="labelClass">
-                <p class="pLogin">Username</p>
-                <input class="inClass" type="text" onChange={e => setUserName(e.target.value)} required />
-                { dispErr && <p>Username not found</p>}
+                <p class="pLogin">Email</p>
+                <input class="inClass" type="email" onChange={e => setUserName(e.target.value)} required />
+                { dispErr && <p>User with email not found</p>}
                 </label>
                 <div class="submitDiv">
                 <button type="submit" class="submitButton">SUBMIT</button>
                 </div>
+                <Link to="/">
+                  Cancel
+                </Link>
             </form>
         </div>
     );
