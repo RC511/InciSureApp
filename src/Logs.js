043f7241d4e534853f3aa1bd32d2ccc31dad467a
HelenaLiu29 <helenaliu929@gmail.com>
2023-10-20 15:21:07 +0800

    20231020_1

--- src/Logs.js
+++ src/Logs.js
@@ -1,17 +1,46 @@
 import NavBarItem from "./NavBarItem";
 import './Logs.css';
-import { Helmet } from 'react-helmet';
-import tempPlot from './assets/tempPlot.png';
-import impPlot from './assets/impPlot.png';
+// import { Helmet } from 'react-helmet';
+// import tempPlot from './assets/tempPlot.png';
+// import impPlot from './assets/impPlot.png';
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { useState } from "react";
+import { tempData } from "./utils/tempData";
+import { impData } from "./utils/impData";
+// import LineChart from "./utils/LineChart";
+import TempChart from "./utils/TempChart";
+import ImpChart from "./utils/ImpChart";
+
+Chart.register(CategoryScale);
 
 export default function Logs() {
+    
+    const [chartData1, setChartData1] = useState({
+        labels: tempData.map((data) => data.year), 
+        datasets: [
+          {
+            label: "Users Gained ",
+            data: tempData.map((data) => data.userGain),
+            borderColor: "red",
+            borderWidth: 2
+          }
+        ]
+    });
+    const [chartData2, setChartData2] = useState({
+        labels: impData.map((data) => data.year), 
+        datasets: [
+          {
+            label: "Users Gained ",
+            data: impData.map((data) => data.userLost),
+            borderColor: "blue",
+            borderWidth: 2,
+          }
+        ]
+    });
+    
     return(
         <div class="back">
-            <Helmet>
-                <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" />
-                <script defer src="https://pyscript.net/latest/pyscript.js"></script>
-            </Helmet>
-            <br></br>
             <div class="navBar">
                 {NavBarItem(2)}
             </div>
@@ -26,18 +55,13 @@ export default function Logs() {
                 </main>
                 <main class="containerPlot">
                     <div class="tempPlot">
-                        <img src={tempPlot}/>
+                        <TempChart chartData={chartData1} />
                     </div>
                     <div class="tempPlot">
-                        <img src={impPlot}/>
+                        <ImpChart chartData={chartData2} />
                     </div>
                 </main>
             </main>
-            <py-script src='./PlotTemp.py'>
-                print("hey")
-                {/* python PlotTemp.py
-                python PlotImp.py */}
-            </py-script>
         </div>
     )
 }
---
+++ src/utils/TempChart.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+
+function TempChart({ chartData }) {
+  return (
+    <div className="chart-container">
+      <h2 style={{ textAlign: "center" }}>Temperature</h2>
+      <Line
+        data={chartData}
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: "Wound temperature for previous 24 hours"
+            },
+            legend: {
+              display: false
+            }
+          }
+        }}
+      />
+    </div>
+  );
+}
+
+export default TempChart;
