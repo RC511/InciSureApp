7491578ef8b54044d6649f38d37dd454f562768f
HelenaLiu29 <helenaliu929@gmail.com>
2023-10-27 12:48:48 +0800

    20231027_1
    Login page => flex

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,10 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './Login.css';
-import { Link, useLocation } from 'react-router-dom';
-import ChangePass from './ChangePass';
-import { initializeApp } from "firebase/app";
+import { Link } from 'react-router-dom';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import app from './firebase';
-
-function useQuery() {
-  const location = useLocation();
-  return new URLSearchParams(location.search);
-}
+import app from './firebase.js';
+import logo from './assets/logo2.png';
 
 export default function Login({ setToken }) {
   const [username, setUserName] = useState();
@@ -18,8 +12,6 @@ export default function Login({ setToken }) {
   const [remember, setRemember] = useState(false);
 
   const auth = getAuth(app);
-  const oobCode = useQuery().get('oobCode');
-  const mode = useQuery().get('mode');
 
   const handleSubmit = async e => {
     e.preventDefault();
@@ -36,40 +28,64 @@ export default function Login({ setToken }) {
     });
   }
 
-  if (mode === "resetPassword" && oobCode)
-  {
-    return <ChangePass oobCode = {oobCode}/>
-  }
-  else
-  {
-    return(
-      <div class="logDiv">
-        <br></br>
-        <h1 class="login">Please Log In</h1>
+  return(
+    <div class="logDiv">
+      <main class="containerLogin">
+        <div>
+          <img src={logo} class="loginLogo"/>
+        </div>
+        <div class="login">
+          Log in to your account :D
+        </div>
         <form onSubmit={handleSubmit}>
-          <label class="labelClass">
-            <p class="pLogin">Email</p>
+          <main class="labelClass">
+            <div class="pLogin">Email</div>
             <input class="inClass" type="email" onChange={e => setUserName(e.target.value)} required />
-          </label>
-          <label class="labelClass">
-            <p class="pLogin">Password</p>
+          </main>
+          <main class="labelClass">
+            <div class="pLogin">Password</div>
             <input class="inClass" type="password" onChange={e => setPassword(e.target.value)} required />
-          </label>
-          <br></br>
-          <span class="passSpan">
-            <input class="remembBox" type="checkbox" id="rememberme" name="rememberme" onChange={e =>setRemember(e.target.checked)} />
-            <label class="remembText" for="rememberme">Remember credentials</label>
+          </main>
+          <main class="bottomLogin">
+            <div class="rememb">
+              <input type="checkbox" id="rememberme" name="rememberme" onChange={e =>setRemember(e.target.checked)} />
+              <label class="remembText" for="rememberme">Remember credentials</label>
+            </div>
             <Link class="forgotPass" to="/forgotpswd">
               Forgot password
             </Link>
-          </span>
+          </main>
           <div class="submitDiv">
-            <button type="submit" class="submitButton">LOG IN</button>
+            <button type="submit" class="submitButton bg-light-blue hover:bg-white">LOG IN</button>
           </div>
         </form>
-      </div>
-    )
-  }
+      </main>
+
+      {/* <br></br>
+      <h1 class="login">Please Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <label class="labelClass">
+          <p class="pLogin">Email</p>
+          <input class="inClass" type="email" onChange={e => setUserName(e.target.value)} required />
+        </label>
+        <label class="labelClass">
+          <p class="pLogin">Password</p>
+          <input class="inClass" type="password" onChange={e => setPassword(e.target.value)} required />
+        </label>
+        <br></br>
+        <span class="passSpan">
+          <input class="remembBox" type="checkbox" id="rememberme" name="rememberme" onChange={e =>setRemember(e.target.checked)} />
+          <label class="remembText" for="rememberme">Remember credentials</label>
+          <Link class="forgotPass" to="/forgotpswd">
+            Forgot password
+          </Link>
+        </span>
+        <div class="submitDiv">
+          <button type="submit" class="submitButton">LOG IN</button>
+        </div>
+      </form> */}
+    </div>
+  )
 }
 
 Login.propTypes = {
